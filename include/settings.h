8f58e9334ca3fb09c1be29d6ec8050df1a995a50
ThomasClb <thomas.caleb@hotmail.fr>
2022-07-02 13:46:31 +0200

    add system choice between EARTH_MOON (default) and SUN_EARTH-MOON

--- include/settings.h
+++ include/settings.h
@@ -11,10 +11,22 @@
 #define SIZE_VECTOR_PL 4 // Size of a state vector (2 positions, 2 velocities)
 #define SEC2DAYS (1.0/(24.*3600)) // Conversion from seconds to days [days.s^-1]
 
-// Define dynamical system
+// Define dynamical system (Uncomment to use SUN EARTH-MOON model, if not it is EARTH MOON)
+// #define SUN_EARTH_MOON
+
+// EARTH MOON
+#ifndef SUN_EARTH_MOON
 #define MU_1 (3.9860043543609598e5) // Mass parameter of the first primary [km^3.s^-2]
 #define MU_2 (4.9028000661637961e3) // Mass parameter of the second primary [km^3.s^-2]
 #define SMA (384399) // Distance between the two primaries [km]
+
+// SUN EARTH-MOON
+#else SUN_EARTH_MOON
+#define MU_1 (1.327124400419393e11) // Mass parameter of the first primary [km^3.s^-2]
+#define MU_2 (4.035032355022598e5) // Mass parameter of the second primary [km^3.s^-2]
+#define SMA (149598261.1504425) // Distance between the two primaries [km]
+
+#endif
 
 // Define normalization units
 #define MU (MU_2 / (MU_1 + MU_2)) // Mass ratio of the primaries [-]
